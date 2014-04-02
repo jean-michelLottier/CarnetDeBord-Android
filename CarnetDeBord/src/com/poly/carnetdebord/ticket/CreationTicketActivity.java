0210903bb6b320fe2fe83be3486a2b7af286362b
patrick008 <patrick.mascary@polymtl.ca>
2014-04-02 17:26:13 -0400

    ajout de la page creation de billet et activity associee

---
+++ CarnetDeBord/src/com/poly/carnetdebord/ticket/CreationTicketActivity.java
@@ -0,0 +1,9 @@
+package com.poly.carnetdebord.ticket;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class CreationTicketActivity extends Activity {
+
+}
